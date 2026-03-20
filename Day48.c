c4fe7b0cfb9144d3c8ee72c965c553f684279ea4
Vaishnavi Dobhal <dobhalvaishnavii74@gmail.com>
2026-03-20 19:45:49 +0530

    Day48

---
+++ Day48.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// structure of node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// function to create new node
+struct Node* newNode(int data) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+// function to count leaf nodes
+int countLeafNodes(struct Node* root) {
+    if (root == NULL)
+        return 0;
+
+    if (root->left == NULL && root->right == NULL)
+        return 1;
+
+    return countLeafNodes(root->left) + countLeafNodes(root->right);
+}
+
+// main function
+int main() {
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+    printf("Leaf nodes: %d\n", countLeafNodes(root));
+
+    return 0;
+}
